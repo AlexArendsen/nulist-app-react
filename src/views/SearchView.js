b01322866fbf742040311db3c9699a29a3e9984e
Alex Arendsen <alex.arendsen.7@gmail.com>
2019-07-04 17:15:46 -0400

    Search now supports #tags and @aliases

--- src/views/SearchView.js
+++ src/views/SearchView.js
@@ -6,19 +6,46 @@ import ItemList from '../components/ItemList';
 import { Routes } from '../values/routes';
 import { searchItems } from '../redux/actions/itemActions';
 
+const TokenTypes = {
+    Tag: 'TAG',
+    Alias: 'ALIAS',
+    Title: 'TITLE',
+}
+
 const parseQuery = (query) => {
     // TODO -- Make this full-featured, think about moving to reducer
     if (!query) return []
-    const preds = query.split(' ')
-        .map(token => new RegExp(token, 'i'))
+
+    const withType = (source) => {
+        const r = (type) => ({ value: source, type })
+
+        switch(source[0]) {
+            case '#': return r(TokenTypes.Tag);
+            case '@': return r(TokenTypes.Alias);
+            default: return r(TokenTypes.Title);
+        }
+    }
+
+    const tokens = query.split(' ').filter(x => !!x && x.length).map(withType);
+    const take = (type) => tokens.filter(t => t.type === type)
+    const m = (v, d = {}) => v || d
+
+    const matchTitle = take(TokenTypes.Title)
+        .map(token => new RegExp(token.value, 'i'))
         .map(pattern => ((item) => pattern.test(item.title)))
-    console.log('returngning preds:', preds)
-    return preds;
+    const matchTag = take(TokenTypes.Tag)
+        .map(token => new RegExp(token.value.slice(1), 'i'))
+        .map(pattern => ((item) => pattern.test(m(m(item.props).tags, ''))))
+    const matchAlias = take(TokenTypes.Alias)
+        .map(token => ((item) => m(m(item.props).alias, '') === token.value))
+
+    const predicates = [ ...matchTitle, ...matchTag, ...matchAlias ]
+    console.log('returning preds:', predicates)
+    return predicates
 }
 
 const itemMatchesPredicates = (item, predicates) => {
     if (!predicates.length) return false;
-    //return predicates.some(p => p(item))
     return predicates.every(p => p(item))
 }
 
@@ -45,6 +72,7 @@ class SearchView extends Component {
 
                 <Paper>
                     <ItemList
+                        enableItemQuickMenu
                         items={ this.props.results.slice(0, 50) }
                         onItemClick={ i => this.props.history.push(Routes.Item(i._id)) } />
                 </Paper>
